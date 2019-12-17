a49b3bb0f08f75cea91a8f51be9582853e2d300c
zhuph <1038190357@qq.com>
2019-12-17 23:28:21 +0800

    初始化Python笔记&&函数使用

---
+++ 04_函数/zph_06_函数的返回值.py
@@ -0,0 +1,12 @@
+def sum_2_num(num1,num2):
+    """对两个数字的求和"""
+
+    result= num1 + num2
+
+    return result
+    # return 表示返回，后续的代码都不会被执行
+    # num = 100
+
+sum_result = sum_2_num(10, 20)
+
+print("计算结果： %d" % sum_result)
---
+++ 04_函数/zph_08_打印分隔线.py
@@ -0,0 +1,19 @@
+def print_line1():
+
+    print("*" * 50)
+
+print_line1()
+
+
+def print_line2(char):
+
+    print(char * 50)
+
+print_line2("-")
+
+
+def print_line3(char,times):
+
+    print(char * times)
+
+print_line3("=", 50)
